69c97358a1efe485e92bf8dd09588f11b0e4bd34
AliceAAtalla <aliceatalla36@gmail.com>
2020-06-13 09:25:51 -0300

    1

--- exercises_11.1/test/exercise_1.test.js
+++
@@ -1,9 +0,0 @@
-const uppercase = require('../script/exercise_1')
-
-test('Transforma as letras de uma palavra em letras maiúsculas', (done) => {
-  expect.assertions(1);
-  uppercase('test', (str) => {
-    expect(str).toBe('TEST');
-    done();
-  });
-});
--- exercises_11.1/test/exercise_2.test.js
+++
@@ -1,6 +0,0 @@
-const getUserName = require('../script/exercises_2-3');
-
-test('Promisse test', () => {
-  expect(getUserName(4)).resolves.toEqual('Mark');
-  expect(getUserName(1)).rejects.toEqual({ 'error': 'User with 1 not found.' })
-})
